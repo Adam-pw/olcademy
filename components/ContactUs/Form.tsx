47f79a35a07ecf403f17ec0323033972ec862619
Adam-pw <adampithewan@gmail.com>
2023-03-08 15:15:47 +0530

    Some UI changes

--- components/ContactUs/Form.tsx
+++ components/ContactUs/Form.tsx
@@ -1,47 +1,56 @@
-import React from "react"
-import { useState } from 'react';
-import { TextInput, createStyles, Button, Textarea } from '@mantine/core';
-import { db } from '../../Firebase/config'
+import React from "react";
+import { useState } from "react";
+import {
+  TextInput,
+  createStyles,
+  Button,
+  Textarea,
+  Paper,
+} from "@mantine/core";
+import { db } from "../../Firebase/config";
 import { collection, addDoc } from "firebase/firestore";
-import styles from './Contact.module.scss'
+import styles from "./Contact.module.scss";
 
-const useStyles = createStyles((theme, { floating }: { floating: boolean }) => ({
-  root: {
-    position: 'relative',
-    margin: '0vw 15vw 3vw 15vw',
-  },
+const useStyles = createStyles(
+  (theme, { floating }: { floating: boolean }) => ({
+    root: {
+      position: "relative",
+      margin: "0vw 15vw 2vw 15vw",
+    },
 
-  label: {
-    position: 'absolute',
-    zIndex: 2,
-    top: 7,
-    left: theme.spacing.sm,
-    pointerEvents: 'none',
-    color: floating
-      ? theme.colorScheme === 'dark'
-        ? theme.white
-        : theme.black
-      : theme.colorScheme === 'dark'
+    label: {
+      position: "absolute",
+      zIndex: 2,
+      top: 7,
+      left: theme.spacing.sm,
+      pointerEvents: "none",
+      color: floating
+        ? theme.colorScheme === "dark"
+          ? theme.white
+          : theme.black
+        : theme.colorScheme === "dark"
         ? theme.colors.dark[3]
         : theme.colors.gray[5],
-    transition: 'transform 150ms ease, color 150ms ease, font-size 150ms ease',
-    transform: floating ? `translate(-${theme.spacing.sm}px, -28px)` : 'none',
-    fontSize: floating ? theme.fontSizes.xs : theme.fontSizes.sm,
-    fontWeight: floating ? 500 : 400,
-  },
-
-  required: {
-    transition: 'opacity 150ms ease',
-    opacity: floating ? 1 : 0,
-  },
-
-  input: {
-    '&::placeholder': {
-      transition: 'color 150ms ease',
-      color: !floating ? 'transparent' : undefined,
+      transition:
+        "transform 150ms ease, color 150ms ease, font-size 150ms ease",
+      transform: floating ? `translate(-${theme.spacing.sm}px, -28px)` : "none",
+      fontSize: floating ? theme.fontSizes.xs : theme.fontSizes.sm,
+      fontWeight: floating ? 500 : 400,
     },
-  },
-}));
+
+    required: {
+      transition: "opacity 150ms ease",
+      opacity: floating ? 1 : 0,
+    },
+
+    input: {
+      "&::placeholder": {
+        transition: "color 150ms ease",
+        color: !floating ? "transparent" : undefined,
+      },
+    },
+  })
+);
 
 export default function Formcomp() {
   const [email, setEmail] = useState<string>();
@@ -50,7 +59,7 @@ export default function Formcomp() {
   const [query, setQuery] = useState<string>();
   const [subject, setSubject] = useState<string>();
 
-  const addContact = (event: { preventDefault: () => void; }) => {
+  const addContact = (event: { preventDefault: () => void }) => {
     event.preventDefault();
     addDoc(collection(db, "contactus"), {
       email: email,
@@ -64,85 +73,93 @@ export default function Formcomp() {
     setMessage("");
     setQuery("");
     setSubject("");
-  }
+  };
 
   const [focused, setFocused] = useState(false);
-  const [value, setValue] = useState('');
-  const { classes } = useStyles({ floating: value.trim().length !== 0 || focused });
+  const [value, setValue] = useState("");
+  const { classes } = useStyles({
+    floating: value.trim().length !== 0 || focused,
+  });
 
   return (
     <>
       <div className={styles.main}>
-        <div className={styles.left}><b> Welcome to Olcademy </b></div>
+        <div className={styles.left}>
+          <b> Welcome to Olcademy </b>
+        </div>
         <div className={styles.right}>
+          <Paper withBorder shadow="md" p={10} mt={10} radius="md">
           <h1 className={styles.heading}>Contact us page :</h1>
           <br></br>
-          <TextInput
-            label="Your Email"
-            placeholder="Example: xyz@gmail.com"
-            required
-            classNames={classes}
-            value={email}
-            onChange={(event) => setEmail(event.currentTarget.value)}
-            onFocus={() => setFocused(true)}
-            onBlur={() => setFocused(false)}
-            mt="md"
-            radius="lg"
-            autoComplete="nope"
-          />
-          <TextInput
-            label="Your Name"
-            placeholder="Example Bill Gates"
-            required
-            classNames={classes}
-            value={name}
-            radius="lg"
-            onChange={(event) => setName(event.currentTarget.value)}
-            onFocus={() => setFocused(true)}
-            onBlur={() => setFocused(false)}
-            mt="md"
-            autoComplete="nope"
-          />
-          <TextInput
-            label="Subject"
-            placeholder="Example Social Science"
-            required
-            classNames={classes}
-            value={subject}
-            radius="lg"
-            onChange={(event) => setSubject(event.currentTarget.value)}
-            onFocus={() => setFocused(true)}
-            onBlur={() => setFocused(false)}
-            mt="md"
-            autoComplete="nope"
-          />
-          <Textarea
-            label="Message you want to convey"
-            placeholder="Anything you want to say"
-            required
-            classNames={classes}
-            value={message}
-            onChange={(event) => setMessage(event.currentTarget.value)}
-            onFocus={() => setFocused(true)}
-            onBlur={() => setFocused(false)}
-            mt="md"
-            radius="lg"
-            autoComplete="nope"
-          />
-          <Textarea
-            label="Any Query"
-            placeholder="Any questions ? ? :)"
-            radius="lg"
-            required
-            classNames={classes}
-            value={query}
-            onChange={(event) => setQuery(event.currentTarget.value)}
-            onFocus={() => setFocused(true)}
-            onBlur={() => setFocused(false)}
-            mt="md"
-            autoComplete="nope"
-          />
-          <Button radius="lg" className={styles.button} onClick={addContact}>Submit</Button>
+            <TextInput
+              label="Your Email"
+              placeholder="Example: xyz@gmail.com"
+              required
+              classNames={classes}
+              value={email}
+              onChange={(event) => setEmail(event.currentTarget.value)}
+              onFocus={() => setFocused(true)}
+              onBlur={() => setFocused(false)}
+              mt="md"
+              radius="lg"
+              autoComplete="nope"
+            />
+            <TextInput
+              label="Your Name"
+              placeholder="Example Bill Gates"
+              required
+              classNames={classes}
+              value={name}
+              radius="lg"
+              onChange={(event) => setName(event.currentTarget.value)}
+              onFocus={() => setFocused(true)}
+              onBlur={() => setFocused(false)}
+              mt="md"
+              autoComplete="nope"
+            />
+            <TextInput
+              label="Subject"
+              placeholder="Example Social Science"
+              required
+              classNames={classes}
+              value={subject}
+              radius="lg"
+              onChange={(event) => setSubject(event.currentTarget.value)}
+              onFocus={() => setFocused(true)}
+              onBlur={() => setFocused(false)}
+              mt="md"
+              autoComplete="nope"
+            />
+            <Textarea
+              label="Message you want to convey"
+              placeholder="Anything you want to say"
+              required
+              classNames={classes}
+              value={message}
+              onChange={(event) => setMessage(event.currentTarget.value)}
+              onFocus={() => setFocused(true)}
+              onBlur={() => setFocused(false)}
+              mt="md"
+              radius="lg"
+              autoComplete="nope"
+            />
+            <Textarea
+              label="Any Query"
+              placeholder="Any questions ? ? :)"
+              radius="lg"
+              required
+              classNames={classes}
+              value={query}
+              onChange={(event) => setQuery(event.currentTarget.value)}
+              onFocus={() => setFocused(true)}
+              onBlur={() => setFocused(false)}
+              mt="md"
+              autoComplete="nope"
+            />
+            <Button radius="lg" className={styles.button} onClick={addContact}>
+              Submit
+            </Button>
+          </Paper>
         </div>
       </div>
     </>
--- components/Login/Logincomp.tsx
+++ components/Login/Logincomp.tsx
@@ -1,58 +1,88 @@
 import {
-    TextInput,
-    PasswordInput,
-    Checkbox,
-    Anchor,
-    Paper,
-    Title,
-    Text,
-    Container,
-    Group,
-    Button,
-  } from '@mantine/core';
-  import { DatePicker } from '@mantine/dates';
-  
-  export function Logincomp() {
-    return (
-      <Container size={420} my={40}>
-        <Title
-          align="center"
-          sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+} from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
+import { useState } from "react";
+
+export function Logincomp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState();
+  const [gender, setGender] = useState<Boolean>();
+  const [ password, setPassword ] = useState("");
+
+  return (
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Create Your account
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        {"Let's dive in at Olcademy"}{" "}
+        <Anchor<"a">
+          href="#"
+          size="sm"
+          onClick={(event) => event.preventDefault()}
         >
-          Create Your account
-        </Title>
-        <Text color="dimmed" size="sm" align="center" mt={5}>
-          {"Let's dive in at Olcademy"}{' '}
-          <Anchor<'a'> href="#" size="sm" onClick={(event) => event.preventDefault()}>
-            Create account
+          Create account
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput
+          label="Name"
+          placeholder="Bill gates"
+          required
+          value={name}
+          onChange={(event) => setName(event.currentTarget.value)}
+        />
+        <br></br>
+        <TextInput label="Email" placeholder="xyz@gmail.com" required value={email}
+          onChange={(event) => setEmail(event.currentTarget.value)}/>
+        <br></br>
+        <DatePicker
+          placeholder="Enter Date Of Birth"
+          label="Birth date"
+          withAsterisk
+        />
+        <br></br>
+        Gender :
+        <Checkbox label="Male" />
+        <Checkbox label="Female" />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Group position="apart" mt="md">
+          <Checkbox label="Remember me" />
+          <Anchor<"a">
+            onClick={(event) => event.preventDefault()}
+            href="#"
+            size="sm"
+          >
+            Forgot password?
           </Anchor>
-        </Text>
-  
-        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-          <TextInput label="Name" placeholder="Bill gates" required />
-          <br></br> 
-          <TextInput label="Email" placeholder="xyz@gmail.com" required />
-          <br></br>
-          <DatePicker placeholder="Enter Date Of Birth" label="Birth date" withAsterisk />
-          <br></br>
-          Gender : 
-          <Checkbox
-          label="Male"
-        />
-          <Checkbox
-          label="Female"
-        />
-          <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-          <Group position="apart" mt="md">
-            <Checkbox label="Remember me" />
-            <Anchor<'a'> onClick={(event) => event.preventDefault()} href="#" size="sm">
-              Forgot password?
-            </Anchor>
-          </Group>
-          <Button fullWidth mt="xl">
-            Sign in
-          </Button>
-        </Paper>
-      </Container>
-    );
-  }
+        </Group>
+        <Button fullWidth mt="xl">
+          Sign in
+        </Button>
+      </Paper>
+    </Container>
+  );
+}
